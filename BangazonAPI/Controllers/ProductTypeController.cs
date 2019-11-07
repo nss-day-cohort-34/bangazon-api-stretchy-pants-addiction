8f060a3798f470163043828006289c536927d679
unknown <mstiles01@icloud.com>
2019-11-07 13:35:48 -0600

    ProductType CRUD created, ProductTypeTest tested up to modify

---
+++ BangazonAPI/Controllers/ProductTypeController.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BangazonAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductTypeController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public ProductTypeController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        private SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        // GET api/producttypes
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+
+                    cmd.CommandText = @"SELECT *
+                                    FROM ProductType";
+                    SqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+                    List<ProductTypes> producttypes = new List<ProductTypes>();
+                    while (reader.Read())
+                    {
+                        ProductTypes producttype = new ProductTypes
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+
+                        };
+
+                        producttypes.Add(producttype);
+                    }
+
+                    reader.Close();
+
+                    return Ok(producttypes);
+                }
+            }
+        }
+
+        // GET api/products/5
+        [HttpGet("{id}", Name = "GetProductType")]
+        public async Task<IActionResult> Get(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT ID, Name
+                                    FROM ProductType
+                                    WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+                    ProductTypes producttypes = null;
+                    if (reader.Read())
+                    {
+                        producttypes = new ProductTypes
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            
+                        };
+                    }
+
+                    reader.Close();
+
+                    return Ok(producttypes);
+                }
+            }
+        }
+
+        // POST api/customers
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] ProductTypes producttype)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    // More string interpolation
+                    cmd.CommandText = @"
+                        INSERT INTO ProductType (Name)
+                        OUTPUT INSERTED.Id
+                        VALUES (@Name)
+                    ";
+                    cmd.Parameters.Add(new SqlParameter("@Name", producttype.Name));
+                    
+
+                    producttype.Id = (int)await cmd.ExecuteScalarAsync();
+
+                    return CreatedAtRoute("GetProductType", new { id = producttype.Id }, producttype);
+                }
+            }
+        }
+
+        // PUT api/product/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] ProductTypes producttypes)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"
+                            UPDATE ProductTypes
+                            SET Title = @Name,
+                           
+                            WHERE Id = @id
+                        ";
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+                        cmd.Parameters.Add(new SqlParameter("@Name", producttypes.Name));
+                       
+                        int rowsAffected = await cmd.ExecuteNonQueryAsync();
+
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+
+                        throw new Exception("No rows affected");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (!ProductTypeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        // DELETE api/product/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"DELETE FROM ProductTypes WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+                        throw new Exception("No rows affected");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (!ProductTypeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        private bool ProductTypeExists(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id FROM ProductTypes WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    return reader.Read();
+                }
+            }
+        }
+    }
+}
---
+++ TestBangazonAPI/ProductTypeTests.cs
@@ -0,0 +1,159 @@
+﻿using BangazonAPI.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestBangazonAPI
+{
+    public class ProductTypeTests
+    {
+
+        [Fact]
+        public async Task Test_Get_All_ProductTypes()
+        {
+
+            using (var client = new APIClientProvider().Client)
+            {
+                /*
+                    ARRANGE
+                */
+
+
+                /*
+                    ACT
+                */
+                var response = await client.GetAsync("/api/ProductType");
+
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var ProductTypeList = JsonConvert.DeserializeObject<List<ProductTypes>>(responseBody);
+
+                /*
+                    ASSERT
+                */
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.True(ProductTypeList.Count > 0);
+            }
+        }
+
+        [Fact]
+        public async Task Test_Get_One_ProductType()
+        {
+
+            using (var client = new APIClientProvider().Client)
+            {
+                /*
+                    ARRANGE
+                */
+
+
+                /*
+                    ACT
+                */
+                var response = await client.GetAsync("/api/producttype/1");
+
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var ProductType = JsonConvert.DeserializeObject<ProductTypes>(responseBody);
+
+                /*
+                    ASSERT
+                */
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.NotNull(ProductType);
+            }
+        }
+        [Fact]
+        public async Task Test_Post_ProductType()
+        {
+            /*
+                Generate a new instance of an HttpClient that you can
+                use to generate HTTP requests to your API controllers.
+                The `using` keyword will automatically dispose of this
+                instance of HttpClient once your code is done executing.
+            */
+            using (var client = new APIClientProvider().Client)
+            {
+                /*
+                    ARRANGE
+                */
+
+                // Construct a new student object to be sent to the API
+                ProductTypes producttype = new ProductTypes
+                {
+                    Name = "Bathroom"
+                };
+
+                // Serialize the C# object into a JSON string
+                var producttypeJSON = JsonConvert.SerializeObject(producttype);
+
+
+                /*
+                    ACT
+                */
+
+                // Use the client to send the request and store the response
+                var response = await client.PostAsync(
+                    "/api/producttype",
+                    new StringContent(producttypeJSON, Encoding.UTF8, "application/json")
+                );
+
+                // Store the JSON body of the response
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                // Deserialize the JSON into an instance of Animal
+                var newproducttype = JsonConvert.DeserializeObject<ProductTypes>(responseBody);
+
+
+                /*
+                    ASSERT
+                */
+
+                Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+                Assert.Equal("Bathroom", newproducttype.Name);
+
+            }
+        }
+
+
+       [Fact]
+        public async Task Test_Modify_ProductType()
+        {
+
+            using (var client = new APIClientProvider().Client)
+            {
+               
+                ProductTypes modifiedJack = new ProductTypes
+                {
+                    Name = "Jack",
+
+                };
+                var modifiedJackAsJSON = JsonConvert.SerializeObject(modifiedJack);
+
+                var response = await client.PutAsync(
+                    "/api/animals/1",
+                    new StringContent(modifiedJackAsJSON, Encoding.UTF8, "application/json")
+                );
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+
+
+            
+                var getJack = await client.GetAsync("/api/animals/1");
+                getJack.EnsureSuccessStatusCode();
+
+                string getJackBody = await getJack.Content.ReadAsStringAsync();
+                ProductTypes newJack = JsonConvert.DeserializeObject<ProductTypes>(getJackBody);
+
+                Assert.Equal(HttpStatusCode.OK, getJack.StatusCode);
+                Assert.Equal(newAge, newJack.Age);
+            }
+        }
+    }
+}
+
