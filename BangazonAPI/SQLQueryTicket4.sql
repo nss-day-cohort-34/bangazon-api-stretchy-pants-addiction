4f91041c81b50c79972157fe5982252aa70237ea
curtis <crutchc@yahoo.com>
2019-11-07 10:49:12 -0600

    Have basic CRUD on Order

---
+++ BangazonAPI/SQLQueryTicket4.sql
@@ -0,0 +1,7 @@
+﻿--SELECT ID, CustomerId, PaymentTypeId FROM [Order];
+SELECT o.Id, o.CustomerId, o.PaymentTypeId FROM [Order] o INNER JOIN PaymentType ON [Order].CustomerId = PaymentType.CustomerId;
+--SELECT ID, AcctNumber, CustomerId, Name FROM PaymentType;
+--INSERT INTO [Order] (CustomerId, PaymentTypeId) OUTPUT INSERTED.Id VALUES ( 3, 1)
+--UPDATE [Order] SET CustomerId = 3, PaymentTypeId = 1
+--			WHERE Id = 2;
+--SELECT ID, FirstName, LastName, CreationDate, LastActiveDate FROM Customer;
